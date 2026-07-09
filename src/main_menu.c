aa9d3af2634929ddb18e7cb4770e656967684b48
Paul Demers <paulmdemers@gmail.com>
2026-07-09 01:24:25 -0400

    Skip dry battery main menu warning

--- src/main_menu.c
+++ src/main_menu.c
@@ -56,7 +56,6 @@ static u32 InitMainMenu(bool8 a1);
 static void Task_MainMenuCheckSave(u8 taskId);
 static void Task_MainMenuWaitForSaveErrorAck(u8 taskId);
 static void Task_MainMenuCheckRtc(u8 taskId);
-static void Task_MainMenuWaitForRtcErrorAck(u8 taskId);
 static void Task_MainMenuDraw(u8 taskId);
 static void Task_MainMenuHighlight(u8 taskId);
 static bool8 MainMenuProcessKeyInput(u8 taskId);
@@ -330,30 +329,7 @@ void Task_MainMenuCheckRtc(u8 taskId)
         REG_BLDALPHA = 0;
         REG_BLDY = 7;
 
-        if (!(RtcGetErrorStatus() & RTC_ERR_FLAG_MASK))
-        {
-            gTasks[taskId].func = Task_MainMenuDraw;
-        }
-        else
-        {
-            Menu_DrawStdWindowFrame(2, 14, 27, 19);
-            MenuPrintMessage(gBatteryDryMessage, 3, 15);
-            REG_WIN0H = WIN_RANGE(17, 223);
-            REG_WIN0V = WIN_RANGE(113, 159);
-            gTasks[taskId].func = Task_MainMenuWaitForRtcErrorAck;
-        }
-    }
-}
-
-void Task_MainMenuWaitForRtcErrorAck(u8 taskId)
-{
-    if (Menu_UpdateWindowText())
-    {
-        if (JOY_NEW(A_BUTTON))
-        {
-            Menu_EraseWindowRect(2, 14, 27, 19);
-            gTasks[taskId].func = Task_MainMenuDraw;
-        }
+        gTasks[taskId].func = Task_MainMenuDraw;
     }
 }
 
